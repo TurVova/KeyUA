b94edbc2ac3cae1db924c3af63446069afe76c68
turvova <1989turvova@gmail.com>
2019-05-08 09:40:35 +0300

    add requirements file

--- test_app/templatetags/key_ua_tags.py
+++ test_app/templatetags/key_ua_tags.py
@@ -2,7 +2,3 @@ from django import template
 
 register = template.Library()
 
-@register.inclusion_tag('test_app/user.html')
-def user_profile(user):
-    context = {'user': user}
-    return context
